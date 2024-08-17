1ac48c28ed1538a9cbf95c76ae4e5879bf0d9669
Teeca-Bakkers <tetsuya.katakura.dev@gmail.com>
2024-08-17 19:04:03 +0900

    fix: remove comments

--- main.py
+++ main.py
@@ -14,13 +14,11 @@ game_data = get_schedule.get_game_data(config_schedule_urls)
 with open("src/game_schedule.json", "w", encoding="utf-8") as f:
     json.dump(game_data, f, ensure_ascii=False, indent=4)
 
-"""
 players_data = {}
 for team in config_team_urls:
-  data = get_playes.get_all_data(config_team_urls[team])
-  all_data[team] = data
+  data = get_players.get_all_data(config_team_urls[team])
+  players_data[team] = data
   print("SUCCESS: " + team + "'s data was obtained.")
 
 with open("src/players_by_position.json", "w", encoding="utf-8") as f:
     json.dump(players_data, f, ensure_ascii=False, indent=4)
-"""
